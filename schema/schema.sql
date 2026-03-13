7f8866ca2b6bab09937f51180e404d0ac42350ff
victo29 <victavares2019@gmail.com>
2026-03-13 20:41:31 -0300

    confing(schema): create schema DB

---
+++ schema/schema.sql
@@ -0,0 +1,81 @@
+-- USERS
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- TRACKS
+CREATE TABLE tracks (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    user_id UUID NOT NULL,
+    conteudo TEXT,
+
+    CONSTRAINT fk_tracks_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+-- MODULES
+CREATE TABLE modules (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    track_id INTEGER NOT NULL,
+    order_index INTEGER,
+
+    CONSTRAINT fk_modules_track
+        FOREIGN KEY (track_id)
+        REFERENCES tracks(id)
+        ON DELETE CASCADE
+);
+
+-- VIDEOS
+CREATE TABLE videos (
+    id SERIAL PRIMARY KEY,
+    url TEXT NOT NULL,
+    title VARCHAR(255),
+    module_id INTEGER NOT NULL,
+
+    CONSTRAINT fk_videos_module
+        FOREIGN KEY (module_id)
+        REFERENCES modules(id)
+        ON DELETE CASCADE
+);
+
+-- CONTENTS
+CREATE TABLE contents (
+    id SERIAL PRIMARY KEY,
+    path TEXT NOT NULL,
+    title VARCHAR(255),
+    module_id INTEGER NOT NULL,
+
+    CONSTRAINT fk_contents_module
+        FOREIGN KEY (module_id)
+        REFERENCES modules(id)
+        ON DELETE CASCADE
+);
+
+-- ACTIVITIES
+CREATE TABLE activities (
+    id SERIAL PRIMARY KEY,
+    question TEXT NOT NULL,
+    module_id INTEGER NOT NULL,
+    answered BOOLEAN DEFAULT FALSE,
+    answers JSONB,
+
+    CONSTRAINT fk_activities_module
+        FOREIGN KEY (module_id)
+        REFERENCES modules(id)
+        ON DELETE CASCADE
+);
+
+-- CREATE INDEX idx_tracks_user_id ON tracks(user_id);
+-- CREATE INDEX idx_modules_track_id ON modules(track_id);
+-- CREATE INDEX idx_modules_user_id ON modules(user_id);
+-- CREATE INDEX idx_videos_module_id ON videos(module_id);
+-- CREATE INDEX idx_contents_module_id ON contents(module_id);
+-- CREATE INDEX idx_activities_module_id ON activities(module_id);
